e65fdfe3bcf8c95b75f7d1d2f96c190ddd29e3ff
JhRamirez <150725448+Focus0Wizard@users.noreply.github.com>
2025-09-14 18:25:57 -0400

    feat: cliente y descuento (#5)
    Co-authored-by: SantiSaaUCB <santiago.saavedra@ucb.edu.bo>

---
+++ miniMarketSolid/Domain/Entities/Cliente.cs
@@ -0,0 +1,49 @@
+﻿namespace miniMarketSolid.Domain.Entities
+{
+    public class Cliente
+    {
+        #region Atributos
+        private int idCliente;
+        private string nombre;
+        private string email;
+        private int telefono;
+        #endregion
+
+        #region Propiedades
+        public int IdCliente
+        {
+            get { return idCliente; }
+            set { idCliente = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public int Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        #endregion
+
+        #region Constructores
+        public Cliente()
+        {
+               
+        }
+        public Cliente(int idCliente, string nombre, string email, int telefono)
+        {
+            this.idCliente = idCliente;
+            this.nombre = nombre;
+            this.email = email;
+            this.telefono = telefono;
+        }
+        #endregion
+    }
+}
